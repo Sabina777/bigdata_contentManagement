177da4b8fd158ed7b55845a577240b9fd3dcdfb0
Sabina777 <sabina.prajapati777@gmail.com>
2020-07-28 17:00:13 +0545

    React bootstrap components used in Post

--- client/src/components/layout/Post.js
+++ client/src/components/layout/Post.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-
+import { Card, Button } from "react-bootstrap";
 const Post = ({ name, text, date }) => {
   return (
     <Fragment>
@@ -9,7 +9,7 @@ const Post = ({ name, text, date }) => {
         alt="Loading..."
       /> */}
 
-      <div
+      {/* <div
         style={{
           height: 300,
           width: 500,
@@ -18,7 +18,8 @@ const Post = ({ name, text, date }) => {
           borderColor: "black",
           borderRadius: 16,
           margin: 20,
-          alignItems: "center",
+          padding: 40,
+          justifyContent: "center",
         }}
       >
         <p>Welcome to the post created by</p>
@@ -27,7 +28,20 @@ const Post = ({ name, text, date }) => {
         <p>{text}</p>
         <p>created on:</p>
         <p>{date}</p>
-      </div>
+      </div> */}
+
+      <Card style={{ width: "18rem", margin: 20 }}>
+        <Card.Img
+          variant="top"
+          src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg"
+        />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>{text}</Card.Text>
+          <Card.Text>{date}</Card.Text>
+          <Button variant="primary">Read Details</Button>
+        </Card.Body>
+      </Card>
     </Fragment>
   );
 };
